469f7f3c9a6690f0dcc2ac5fecae975d8b5eb396
Gallardo <ganibal53@bk.ru>
2018-09-29 23:19:15 +0300

    Удаление лишних объектов - чертова VS на mac os

--- FridgeStorageProcessor/DBStore/ISqlStrings.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace DBStore
-{
-    public interface ISqlStrings
-    {
-    }
-}
--- FridgeStorageProcessor/SqlEngine/Query/IQueryParameter.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace SqlEngine.Query
-{
-    public interface IQueryParameter
-    {
-    }
-}
